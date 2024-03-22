ca92cb738f3f5d0aa6ebc15e4711a46f01da851b
CodingMagic <codingmagic4kids@gmail.com>
2024-03-21 21:22:06 -0700

    first

---
+++ AddNums.java
@@ -0,0 +1,24 @@
+// Learnt about Operating Systems - Unix, Linux, MacOS, Windows
+// Class
+
+// Next Class - Datatypes, Variables, Keywords and Operators
+
+
+class AddNums
+{
+   public static int sum (int a, int b)
+   {
+      return (a+b);
+   }
+
+   public static void main (String []args)
+   {
+      int a = 10;
+      int b = 50;
+      System.out.println ( sum (a, b) );
+   }
+} 
+
+
+
+
